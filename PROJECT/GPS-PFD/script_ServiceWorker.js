88556aa488cb237c7a90e21ec456d21adda210ac
candy10243 <kingstone020506@gmail.com>
2025-05-19 00:28:31 +0800

    v0.26 Beta Test

--- PROJECT/GPS-PFD/script_ServiceWorker.js
+++ PROJECT/GPS-PFD/script_ServiceWorker.js
@@ -6,7 +6,7 @@
 	// Declare variables
 	"use strict";
 		// Unsaved
-		const CacheName = "GPS-PFD_v0.25";
+		const CacheName = "GPS-PFD_v0.26";
 
 // Listeners
 	// Service worker (https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/#step-5---add-a-service-worker)
